f1baea8ebacfc3a957007c324a7778d15060d473
yasith <yasith@cs.odu.edu>
2020-04-23 03:24:50 -0400

    model changes

--- 4_train.py
+++ 4_train.py
@@ -7,28 +7,27 @@ from tensorflow import keras as k
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 
-def create_lstm_32(frames, ch_rows, ch_cols, bands):
+def create_lstm_64(frames, ch_rows, ch_cols, bands):
     """
     Generate 32-unit LSTM model
     """
-    reg = k.regularizers.l1_l2(0.001, 0.001)
+    reg = k.regularizers.l1_l2(0.005, 0.005)
     _model = k.models.Sequential(name='asd_lstm_32', layers=[
         k.layers.Input(shape=(frames, ch_rows, ch_cols, bands)),
         k.layers.TimeDistributed(k.layers.GlobalMaxPooling2D(), name='eeg'),  # shape: (frames, bands)
-        k.layers.LSTM(32, return_sequences=True, kernel_regularizer=reg, name='lstm_1'),
-        k.layers.LSTM(32, kernel_regularizer=reg, name='lstm_2'),
-        k.layers.Dense(64, activation='sigmoid', kernel_regularizer=reg, name='dense'),
+        k.layers.LSTM(64, kernel_regularizer=reg, name='lstm_1'),
+        k.layers.Dense(32, activation='sigmoid', kernel_regularizer=reg, name='dense'),
         k.layers.Dense(1, activation='sigmoid', kernel_regularizer=reg, name='prediction')
     ])
-    _model.compile(optimizer=k.optimizers.SGD(learning_rate=0.001, momentum=0.9), loss='binary_crossentropy', metrics=['accuracy'])
+    _model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
     return _model
 
 
-def create_conv_32(frames, ch_rows, ch_cols, bands):
+def create_conv_64(frames, ch_rows, ch_cols, bands):
     """
-    Generate 32-unit 1D Convolution model
+    Generate 64-unit 1D Convolution model
     """
-    reg = k.regularizers.l1_l2(0.001, 0.001)
+    reg = k.regularizers.l1_l2(0.005, 0.005)
     conv_1d_spec = {
         'activation': 'relu',
         'kernel_regularizer': reg,
@@ -37,14 +36,13 @@ def create_conv_32(frames, ch_rows, ch_cols, bands):
     _model = k.models.Sequential(name='asd_conv_32', layers=[
         k.layers.Input(shape=(frames, ch_rows, ch_cols, bands)),
         k.layers.TimeDistributed(k.layers.GlobalMaxPooling2D(), name='eeg'),  # shape: (frames, bands)
-        k.layers.Conv1D(filters=16, kernel_size=4, strides=1, **conv_1d_spec, name='conv_1'),
+        k.layers.Conv1D(filters=32, kernel_size=4, **conv_1d_spec, name='conv_1'),
         k.layers.MaxPooling1D(name='pool_1'),
-        k.layers.Dropout(rate=0.1, name='dropout_1'),
-        k.layers.Conv1D(filters=32, kernel_size=4, strides=1, **conv_1d_spec, name='conv_2'),
+        k.layers.Conv1D(filters=64, kernel_size=4, **conv_1d_spec, name='conv_2'),
         k.layers.GlobalMaxPooling1D(name='pool_2'),
         k.layers.Dense(1, activation='sigmoid', kernel_regularizer=reg, name='prediction')
     ])
-    _model.compile(optimizer=k.optimizers.SGD(learning_rate=0.001, momentum=0.9), loss='binary_crossentropy', metrics=['accuracy'])
+    _model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
     return _model
 
 
@@ -67,7 +65,7 @@ if __name__ == '__main__':
     print('OK')
 
     print('Creating Models...')
-    models = [create_conv_32(*X.shape[1:]), create_lstm_32(*X.shape[1:]), ]
+    models = [create_conv_64(*X.shape[1:]), create_lstm_64(*X.shape[1:]), ]
     print('OK')
 
     print('Evaluating')
