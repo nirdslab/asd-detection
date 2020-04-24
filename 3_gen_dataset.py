2049c2f60b42b2d3fc3debd741f93f4c456e05f3
yasith <yasith@cs.odu.edu>
2020-04-24 01:55:15 -0400

    increase temporal resolution of dataset, reduce data overlaps

--- 3_gen_dataset.py
+++ 3_gen_dataset.py
@@ -27,9 +27,10 @@ if __name__ == '__main__':
     # define parameters to extract temporal slices
     NUM_BANDS = 50  # number of frequency bands in final result
     BANDS = np.arange(NUM_BANDS) + 1  # frequencies (1 Hz - 50 Hz)
-    SLICE_WINDOW = 20  # secs per slice
-    SLICE_STEP = 2  # secs to step to get next slice
-    SLICE_SHAPE = (SLICE_WINDOW, NUM_CH_ROWS, NUM_CH_COLS, NUM_BANDS)
+    SLICE_WINDOW = 10  # secs per slice
+    SLICE_STEP = 5  # secs to step to get next slice
+    K = 2  # slice resolution (in Hz)
+    SLICE_SHAPE = (SLICE_WINDOW * K, NUM_CH_ROWS, NUM_CH_COLS, NUM_BANDS)
 
     # define x, y, and z
     _x = np.empty(shape=(0, *SLICE_SHAPE))  # sample
@@ -50,22 +51,22 @@ if __name__ == '__main__':
         dp = data.loc[p].set_index('Epoch')
         for j, e in enumerate(epochs):
             print(f'{e} ', flush=True, end='')
-            de = dp.loc[e].set_index('T').to_numpy()  # type: np.ndarray # shape: (time, channel)
+            de = dp.loc[e].set_index('T').to_numpy()  # type: np.ndarray # shape: (timestep, channel)
             # wavelet transform on each channel
             transforms = []
             for channel in np.transpose(de):
                 # truncate signal to the nearest 1000
                 max_frame = len(channel) - len(channel) % 1000
                 c, _ = pywt.cwt(data=channel, scales=scales, wavelet=wavelet, sampling_period=DT)  # type: np.ndarray
-                c_reduced = np.amax(c[:, :max_frame].reshape((NUM_BANDS, max_frame // FREQ, FREQ)), axis=-1)  # type: np.ndarray
-                transforms.append(np.transpose(c_reduced))  # shape: (time, band)
+                c_reduced = np.amax(c[:, :max_frame].reshape((NUM_BANDS, max_frame // (FREQ // K), (FREQ // K))), axis=-1)  # type: np.ndarray
+                transforms.append(np.transpose(c_reduced))  # shape: (timestep, band)
             # wavelet decompositions
-            wd = np.stack(transforms, axis=1)  # shape: (time, channel, band)
+            wd = np.stack(transforms, axis=1)  # shape: (timestep, channel, band)
             # stack sliding windows
-            N = (len(wd) - SLICE_WINDOW) // SLICE_STEP
+            N = (len(wd) - (SLICE_WINDOW * K)) // (SLICE_STEP * K)
             # windowed samples
-            ws = [np.roll(wd, -k * SLICE_STEP, axis=0)[:SLICE_WINDOW].reshape(SLICE_SHAPE) for k in range(N)]
-            ds = np.stack(ws, axis=0)  # shape: (sample, time, row, col, band)
+            ws = [np.roll(wd, -k * (SLICE_STEP * K), axis=0)[:(SLICE_WINDOW * K)].reshape(SLICE_SHAPE) for k in range(N)]
+            ds = np.stack(ws, axis=0)  # shape: (sample, timestep, row, col, band)
             _x = np.append(_x, ds, axis=0)
             _y = np.append(_y, np.full((N,), label), axis=0)
             _z = np.append(_z, np.full((N,), score), axis=0)
@@ -89,7 +90,7 @@ if __name__ == '__main__':
     alpha = _x[..., 7:16]  # (8 - 16 Hz)
     beta = _x[..., 15:32]  # (16 - 32 Hz)
     gamma = _x[..., 31:]  # ( >= 32 Hz)
-    _xb = np.stack([np.max(x, axis=-1) for x in [delta, theta, alpha, beta, gamma]], axis=-1).astype(np.float32)  # type: np.ndarray
+    _xb = np.stack([np.max(x, axis=-1) for x in [delta, theta, alpha, beta, gamma]], axis=-1)  # type: np.ndarray
     print('OK')
 
     print('Saving frequency band data')
