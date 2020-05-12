f0343fa211fe6c7a72ff2b3428540282c5281537
yasith <yasith@cs.odu.edu>
2020-05-11 22:06:35 -0400

    add downsampling factor to power spectrum

--- 3_gen_dataset.py
+++ 3_gen_dataset.py
@@ -3,7 +3,6 @@
 import numpy as np
 import pandas as pd
 import pywt
-from matplotlib import pyplot as plt
 
 from info import participants, epochs, sampling_freq
 
@@ -20,15 +19,16 @@ if __name__ == '__main__':
     # define constant values
     NUM_CH_ROWS = 5  # EEG channel rows
     NUM_CH_COLS = 6  # EEG channel columns
-    FREQ = sampling_freq  # sampling frequency
-    DT = 1.0 / FREQ  # sampling period
+    SRC_FREQ = sampling_freq  # sampling frequency
+    TARGET_FREQ = 5  # 5 Hz
+    DT = 1.0 / SRC_FREQ  # sampling period
 
     # define parameters to extract temporal slices
     NUM_BANDS = 50  # number of frequency bands in final result
     BANDS = np.arange(NUM_BANDS) + 1  # frequencies (1 Hz - 50 Hz)
     SLICE_WINDOW = 10  # secs per slice
     SLICE_STEP = 10  # secs to step to get next slice
-    SLICE_SHAPE = (SLICE_WINDOW * FREQ, NUM_CH_ROWS, NUM_CH_COLS, NUM_BANDS)
+    SLICE_SHAPE = (SLICE_WINDOW * TARGET_FREQ, NUM_CH_ROWS, NUM_CH_COLS, NUM_BANDS)
 
     # define x, y, and z
     _x = np.empty(shape=(0, *SLICE_SHAPE))  # sample
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     # wavelet transform properties
     wavelet = 'cmor1.5-1.0'  # complex morlet wavelet (Bandwidth - 1.5 Hz, Center Frequency - 1.0 Hz)
     # wavelet = 'sym9'  # symlet 9 wavelet
-    scales = FREQ / BANDS  # scales corresponding to frequency bands
+    scales = SRC_FREQ / BANDS  # scales corresponding to frequency bands
 
     # generate values for x, y, and z
     print('Generating X, Y, and Z')
@@ -59,18 +59,21 @@ if __name__ == '__main__':
                 # calculate abs square of c to obtain wavelet power spectrum
                 p = np.abs(c) ** 2  # type: np.ndarray
                 # truncate p to avoid partial slices
-                last_t = len(ch) // FREQ
+                last_t = len(ch) // SRC_FREQ
                 last_t -= (last_t - SLICE_WINDOW) % SLICE_STEP
-                timesteps = last_t * FREQ
+                timesteps = last_t * SRC_FREQ
                 l_trim = (len(ch) - timesteps) // 2
                 p = p[:, l_trim:l_trim + timesteps]
+                # down-scale the power spectrum to target frequency (helps to reduce kernel size later)
+                E = SRC_FREQ // TARGET_FREQ
+                p = np.amax(p.reshape((p.shape[0], p.shape[1] // E, E)), axis=-1)
                 # append power of channel to array
                 ch_p.append(p.T)  # shape: (timestep, band)
             # power spectrum
             ps = np.stack(ch_p, axis=1)  # shape: (timestep, channel, band)
             # chunk power spectrum into N slices of SLICE_SHAPE
-            N = (len(ps) - SLICE_WINDOW * FREQ) // (SLICE_STEP * FREQ)
-            ws = [np.roll(ps, -k * SLICE_STEP * FREQ, axis=0)[:(SLICE_WINDOW * FREQ)].reshape(SLICE_SHAPE) for k in range(N)]
+            N = (len(ps) - SLICE_WINDOW * TARGET_FREQ) // (SLICE_STEP * TARGET_FREQ)
+            ws = [np.roll(ps, -k * SLICE_STEP * TARGET_FREQ, axis=0)[:(SLICE_WINDOW * TARGET_FREQ)].reshape(SLICE_SHAPE) for k in range(N)]
             ds = np.stack(ws, axis=0)  # shape: (sample, timestep, row, col, band)
             _x = np.append(_x, ds, axis=0)
             _y = np.append(_y, np.full((N,), label), axis=0)
