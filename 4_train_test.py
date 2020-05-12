a7c2cd5cfe3b89a25283b5a780b63e701a6e3492
yasith <yasith@cs.odu.edu>
2020-05-12 01:55:31 -0400

    update code

--- 4_train_test.py
+++ 4_train_test.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-from typing import List, Tuple
+from typing import List
 
 import numpy as np
 import tensorflow as tf
@@ -13,75 +13,73 @@ import models
 from info import participants, SLICE_SHAPE_BANDS
 
 tf.random.set_seed(42)
-
-
-def train_dev_test_split(_x: np.ndarray, _y: np.ndarray, _z: np.ndarray, r_dev: float, r_test: float, rand_state: int):
-    """
-    train/dev/test split at given ratios
-    """
-    from sklearn.model_selection import train_test_split
-    assert r_dev + r_test < 1
-    r1 = r_dev + r_test
-    r2 = r_test / r1
-    _x_tr, _x_ts, _y_tr, _y_ts, _z_tr, _z_ts = train_test_split(_x, _y, _z, test_size=r1, shuffle=True, random_state=rand_state)
-    _x_dv, _x_ts, _y_dv, _y_ts, _z_dv, _z_ts = train_test_split(_x_ts, _y_ts, _z_ts, test_size=r2, shuffle=True, random_state=rand_state)
-    result = [_x_tr, _x_dv, _x_ts], [_y_tr, _y_dv, _y_ts], [_z_tr, _z_dv, _z_ts]  # type: Tuple[List[np.ndarray], List[np.ndarray], List[np.ndarray]]
-    return result
-
+np.random.seed(42)
 
 if __name__ == '__main__':
     # parse command line arguments
-    training = True
-    testing = True
-    if len(sys.argv) > 2:
+    training = False
+    testing = False
+    if len(sys.argv) != 2:
         print('Arguments: [ train | test ]')
         exit(1)
-    elif len(sys.argv) == 2:
+    else:
         mode = sys.argv[1].strip().lower()
         if mode not in ['train', 'test']:
             print('Arguments: [OPTIONAL] [ train | test ]')
             exit(1)
         if mode == 'train':
-            testing = False
+            training = True
         elif mode == 'test':
-            training = False
+            testing = True
 
     # load dataset
     print('loading dataset...', end=' ', flush=True)
     dataset = np.load('data/data-processed-bands.npz')
-    # randomly split participants
-    X = np.zeros((0, *SLICE_SHAPE_BANDS))  # type: np.ndarray
-    Y = np.zeros(0, )  # type: np.ndarray
-    Z = np.zeros(0, )  # type: np.ndarray
+
+    # train-test-split on participant ID
+    fraction = 0.8
+    num_train = int(len(participants) * fraction)
+    p_train = set(np.random.choice(participants, num_train, replace=False))
+
+    # create test and train data
+    X_TRAIN, X_TEST = np.zeros((0, *SLICE_SHAPE_BANDS)), np.zeros((0, *SLICE_SHAPE_BANDS))  # type: np.ndarray
+    Y_TRAIN, Y_TEST = np.zeros(0, ), np.zeros(0, )  # type: np.ndarray
+    Z_TRAIN, Z_TEST = np.zeros(0, ), np.zeros(0, )  # type: np.ndarray
     for p in participants:
         _x = dataset[f'{p}_x']
-        _y = dataset[f'{p}_y']
-        _z = dataset[f'{p}_z']
-        X = np.append(X, _x, axis=0)
-        Y = np.append(Y, np.full(len(_x), _y), axis=0)
-        Z = np.append(Z, np.full(len(_x), _z), axis=0)
+        _y = np.full(len(_x), dataset[f'{p}_y'])
+        _z = np.full(len(_x), dataset[f'{p}_z'])
+        if p in p_train:
+            X_TRAIN = np.append(X_TRAIN, _x, axis=0)
+            Y_TRAIN = np.append(Y_TRAIN, _y, axis=0)
+            Z_TRAIN = np.append(Z_TRAIN, _z, axis=0)
+        else:
+            X_TEST = np.append(X_TEST, _x, axis=0)
+            Y_TEST = np.append(Y_TEST, _y, axis=0)
+            Z_TEST = np.append(Z_TEST, _z, axis=0)
     print('OK')
-    print(f'X={X.shape}, Y={Y.shape}, Z={Z.shape}')
+    print(f'TRAINING: X={X_TRAIN.shape}, Y={Y_TRAIN.shape}, Z={Z_TRAIN.shape}')
+    print(f'TESTING: X={X_TEST.shape}, Y={Y_TEST.shape}, Z={Z_TEST.shape}')
 
     # normalize x
     print('normalizing X...', end=' ', flush=True)
-    X = (X - np.min(X)) / (np.max(X) - np.min(X))
+    X_MAX = np.maximum(np.amax(X_TRAIN), np.amax(X_TEST))
+    X_MIN = np.minimum(np.amin(X_TRAIN), np.amin(X_TEST))
+    X_TRAIN = (X_TRAIN - X_MIN) / (X_MAX - X_MIN)
+    X_TEST = (X_TEST - X_MIN) / (X_MAX - X_MIN)
     print('OK')
-
-    # spatial localization
-    # print('performing spatial localization...', end=' ', flush=True)
-    # X = tf.nn.max_pool(tf.convert_to_tensor(X, tf.float32), ksize=[1, 3, 2], strides=[1, 1, 2], padding='VALID').numpy()
-    # print('OK')
 
     # generate time-major and channel-major data
     # NOTE: this was done to avoid repeated transposition, which is computationally expensive
     print('creating time-major and channel-major data...', end=' ', flush=True)
     # time-major data
-    DATA_TM = [X, Y, Z]
-    TM_SHAPE = DATA_TM[0].shape[1:]
+    DATA_TM_TRAIN = [X_TRAIN, Y_TRAIN, Z_TRAIN]  # type: List[np.ndarray]
+    DATA_TM_TEST = [X_TEST, Y_TEST, Z_TEST]  # type: List[np.ndarray]
+    TM_SHAPE = DATA_TM_TRAIN[0].shape[1:]
     # channel-major data
-    DATA_CM = [tf.transpose(X, perm=[0, 2, 3, 1, 4]).numpy(), Y, Z]  # type: List[np.ndarray]
-    CM_SHAPE = DATA_CM[0].shape[1:]
+    DATA_CM_TRAIN = [tf.transpose(X_TRAIN, perm=[0, 2, 3, 1, 4]).numpy(), Y_TRAIN, Z_TRAIN]  # type: List[np.ndarray]
+    DATA_CM_TEST = [tf.transpose(X_TEST, perm=[0, 2, 3, 1, 4]).numpy(), Y_TEST, Z_TEST]  # type: List[np.ndarray]
+    CM_SHAPE = DATA_CM_TRAIN[0].shape[1:]
     print('OK')
 
     print('Creating Models...', end=' ', flush=True)
@@ -91,23 +89,23 @@ if __name__ == '__main__':
 
     # training models and specs (model, data, loss)
     models = [
-        (models.conv_nn_tm(*TM_SHAPE), DATA_TM, default_loss),
-        (models.conv_nn_cm(*CM_SHAPE), DATA_CM, default_loss),
-        (models.capsule_nn(*TM_SHAPE), DATA_TM, caps_loss),
-        (models.lstm_nn(*TM_SHAPE), DATA_TM, default_loss),
+        (models.conv_nn_tm(*TM_SHAPE), DATA_TM_TRAIN, DATA_TM_TEST, default_loss),
+        (models.conv_nn_cm(*CM_SHAPE), DATA_CM_TRAIN, DATA_CM_TEST, default_loss),
+        (models.capsule_nn(*TM_SHAPE), DATA_TM_TRAIN, DATA_TM_TEST, caps_loss),
+        (models.lstm_nn(*TM_SHAPE), DATA_TM_TRAIN, DATA_TM_TEST, default_loss),
     ]
     print('OK')
 
     print('Training and Evaluation')
     optimizer = k.optimizers.Adam()
     # iterate each model type
-    for model, [x, y, z], loss in models:
-        y = k.utils.to_categorical(y, num_classes=2)
-        # train-dev-test split at 60-20-20 ratio
-        [x_tr, x_dv, x_ts], [y_tr, y_dv, y_ts], [z_tr, z_dv, z_ts] = train_dev_test_split(x, y, z, r_dev=0.2, r_test=0.2, rand_state=42)
+    model = ... # type: k.Model
+    for model, [x_tr, y_tr, z_tr], [x_te, y_te, z_te], loss in models:
+        y_tr = k.utils.to_categorical(y_tr, num_classes=2)
+        y_te = k.utils.to_categorical(y_te, num_classes=2)
         filepath = f'weights/{model.name}.hdf5'
         # build model
-        model.compile(optimizer=optimizer, loss=loss, loss_weights=[1, 0.01], metrics={'label': 'accuracy', 'score': 'mse'})
+        model.compile(optimizer=optimizer, loss=loss, loss_weights=[1, 0.1], metrics={'label': 'accuracy', 'score': 'mse'})
         model.summary()
         # training phase
         if training:
@@ -115,10 +113,9 @@ if __name__ == '__main__':
             if os.path.exists(filepath):
                 model.load_weights(filepath)
             # train
-            save_best = k.callbacks.ModelCheckpoint(filepath, monitor='val_loss', save_best_only=True, save_weights_only=True)
-            model.fit(x_tr, [y_tr, z_tr], batch_size=64, epochs=200, validation_data=(x_dv, [y_dv, z_dv]), callbacks=[save_best])
-        # testing phase
+            save_best = k.callbacks.ModelCheckpoint(filepath, monitor='val_loss', save_best_only=True, save_weights_only=True, verbose=0)
+            model.fit(x_tr, [y_tr, z_tr], batch_size=64, epochs=200, validation_data=(x_te, [y_te, z_te]), callbacks=[save_best], verbose=2)
         if testing:
             model.load_weights(filepath)
-            model.evaluate(x_ts, [y_ts, z_ts])
+            model.evaluate(x_te, [y_te, z_te], batch_size=64)
     print('Done')
--- models.py
+++ models.py
@@ -38,12 +38,12 @@ def conv_nn_tm(timesteps, ch_rows, ch_cols, bands):
     # == intermediate layer(s) ==
     ml = kl.TimeDistributed(kl.Flatten(), name='eeg')(il)
     # convolution 1
-    ml = kl.Conv1D(filters=64, kernel_size=4, activation='relu', kernel_regularizer='l1_l2', padding='same', name='conv_1')(ml)
+    ml = kl.Conv1D(filters=64, kernel_size=10, activation='relu', kernel_regularizer='l1_l2', padding='same', name='conv_1')(ml)
     ml = kl.MaxPooling1D(name='pool_1')(ml)
     ml = kl.Dropout(0.2, name='dropout_1')(ml)
     ml = kl.BatchNormalization(name='b_norm_1')(ml)
     # convolution 2
-    ml = kl.Conv1D(filters=128, kernel_size=4, activation='relu', kernel_regularizer='l1_l2', padding='same', name='conv_2')(ml)
+    ml = kl.Conv1D(filters=128, kernel_size=10, activation='relu', kernel_regularizer='l1_l2', padding='same', name='conv_2')(ml)
     ml = kl.MaxPooling1D(name='pool_2')(ml)
     ml = kl.Dropout(0.2, name='dropout_2')(ml)
     ml = kl.BatchNormalization(name='b_norm_2')(ml)
@@ -68,12 +68,12 @@ def conv_nn_cm(ch_rows, ch_cols, timesteps, bands):
     # == intermediate layer(s) ==
     ml = kl.Reshape(target_shape=(ch_rows * ch_cols, timesteps, bands), name='eeg')(il)
     # convolution 1
-    ml = kl.TimeDistributed(kl.Conv1D(filters=64, kernel_size=4, activation='relu', kernel_regularizer='l1_l2', padding='same'), name='conv_1')(ml)
+    ml = kl.TimeDistributed(kl.Conv1D(filters=64, kernel_size=10, activation='relu', kernel_regularizer='l1_l2', padding='same'), name='conv_1')(ml)
     ml = kl.TimeDistributed(kl.MaxPooling1D(), name='pool_1')(ml)
     ml = kl.TimeDistributed(kl.Dropout(0.2), name='dropout_1')(ml)
     ml = kl.TimeDistributed(kl.BatchNormalization(), name='b_norm_1')(ml)
     # convolution 2
-    ml = kl.TimeDistributed(kl.Conv1D(filters=128, kernel_size=4, activation='relu', kernel_regularizer='l1_l2', padding='same'), name='conv_2')(ml)
+    ml = kl.TimeDistributed(kl.Conv1D(filters=128, kernel_size=10, activation='relu', kernel_regularizer='l1_l2', padding='same'), name='conv_2')(ml)
     ml = kl.TimeDistributed(kl.MaxPooling1D(), name='pool_2')(ml)
     ml = kl.TimeDistributed(kl.Dropout(0.2), name='dropout_2')(ml)
     ml = kl.TimeDistributed(kl.BatchNormalization(), name='b_norm_2')(ml)
@@ -95,12 +95,12 @@ def capsule_nn(timesteps, ch_rows, ch_cols, bands):
     # == intermediate layer(s) ==
     ml = kl.Reshape(target_shape=(timesteps, ch_rows * ch_cols, bands), name='eeg')(il)
     # initial convolution
-    ml = kl.Conv2D(filters=128, kernel_size=(3, 1), strides=(1, 1), activation='relu', name='conv')(ml)
+    ml = kl.Conv2D(filters=32, kernel_size=(10, 1), strides=(2, 1), activation='relu', name='conv')(ml)
     # convert to capsule domain
-    ml = ConvCaps2D(filters=16, filter_dims=8, kernel_size=(3, 1), strides=(1, 1), name='conv_caps')(ml)
+    ml = ConvCaps2D(filters=8, filter_dims=4, kernel_size=(5, 1), strides=(2, 1), name='conv_caps')(ml)
     ml = kl.Lambda(squash)(ml)
     # dense capsule layer with dynamic routing
-    ml = DenseCaps(caps=2, caps_dims=16, routing_iter=3, name='dense_caps')(ml)
+    ml = DenseCaps(caps=2, caps_dims=8, routing_iter=3, name='dense_caps')(ml)
     ml = kl.Lambda(squash)(ml)
     # select capsule with highest activity
     cl = kl.Lambda(mask_cid)(ml)
