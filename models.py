a92e469a91b05cafab0ffea65dc86784962a663f
Yasith Jayawardana <yasith@cs.odu.edu>
2020-05-14 00:55:33 -0400

    updated weights

--- models.py
+++ models.py
@@ -8,6 +8,39 @@ DROPOUT = 0.4
 REG = 'l1_l2'
 
 
+def DB(name, layers, filters, kernel_size):
+    conv_spec = {'padding': 'same', 'kernel_regularizer': REG}
+
+    def call(_ml):
+        _layers = []
+        for _i in range(layers):
+            # convolution
+            _ml = kl.BatchNormalization(name=f'db_bnrm_{name}_{_i + 1}')(_ml)
+            _ml = kl.ReLU(name=f'db_relu_{name}_{_i + 1}')(_ml)
+            _ml = kl.Conv2D(filters, kernel_size, **conv_spec, name=f'db_conv_{name}_{_i + 1}')(_ml)
+            _ml = kl.Dropout(DROPOUT, name=f'db_drop_{name}_{_i + 1}')(_ml)
+            _layers.append(_ml)
+            if _i > 0: _ml = kl.Concatenate(name=f'c_{name}_{_i + 1}')([*_layers])
+        return _ml
+
+    return call
+
+
+def TB(name, filters):
+    conv_spec = {'padding': 'same', 'kernel_regularizer': REG}
+
+    def call(_ml):
+        # convolution
+        _ml = kl.BatchNormalization(name=f'tb_bnrm_{name}')(_ml)
+        _ml = kl.ReLU(name=f'tb_relu_{name}')(_ml)
+        _ml = kl.Conv2D(filters, (1, 1), **conv_spec, name=f'tb_conv_{name}')(_ml)
+        _ml = kl.Dropout(DROPOUT, name=f'tb_dropout_{name}')(_ml)
+        _ml = kl.AveragePooling2D((2, 1), name=f'tb_pool_{name}')(_ml)
+        return _ml
+
+    return call
+
+
 def conv_nn(timesteps, ch_rows, ch_cols, bands):
     """
     Generate Convolution NN based model
@@ -18,38 +51,6 @@ def conv_nn(timesteps, ch_rows, ch_cols, bands):
     :param bands:
     :return:
     """
-
-    def DB(name, layers, filters, kernel_size):
-        conv_spec = {'padding': 'same', 'kernel_regularizer': REG}
-
-        def call(_ml):
-            _layers = []
-            for _i in range(layers):
-                # convolution
-                _ml = kl.BatchNormalization(name=f'db_bnrm_{name}_{_i + 1}')(_ml)
-                _ml = kl.ReLU(name=f'db_relu_{name}_{_i + 1}')(_ml)
-                _ml = kl.Conv2D(filters, kernel_size, **conv_spec, name=f'db_conv_{name}_{_i + 1}')(_ml)
-                _ml = kl.Dropout(DROPOUT, name=f'db_drop_{name}_{_i + 1}')(_ml)
-                _layers.append(_ml)
-                if _i > 0: _ml = kl.Concatenate(name=f'c_{name}_{_i + 1}')([*_layers])
-            return _ml
-
-        return call
-
-    def TB(name, filters):
-        conv_spec = {'padding': 'same', 'kernel_regularizer': REG}
-
-        def call(_ml):
-            # convolution
-            _ml = kl.BatchNormalization(name=f'tb_bnrm_{name}')(_ml)
-            _ml = kl.ReLU(name=f'tb_relu_{name}')(_ml)
-            _ml = kl.Conv2D(filters, (1, 1), **conv_spec, name=f'tb_conv_{name}')(_ml)
-            _ml = kl.Dropout(DROPOUT, name=f'tb_dropout_{name}')(_ml)
-            _ml = kl.AveragePooling2D((2, 1), name=f'tb_pool_{name}')(_ml)
-            return _ml
-
-        return call
-
     # == input layer(s) ==
     il = kl.Input(shape=(timesteps, ch_rows, ch_cols, bands))
     ml = kl.Reshape((timesteps, ch_rows * ch_cols, bands))(il)
@@ -122,20 +123,23 @@ def capsule_nn(timesteps, ch_rows, ch_cols, bands):
     """
     # == input layer(s) ==
     il = kl.Input(shape=(timesteps, ch_rows, ch_cols, bands))
+    ml = kl.Reshape(target_shape=(timesteps, ch_rows * ch_cols, bands), name='eeg')(il)
 
     # == intermediate layer(s) ==
-    ml = kl.Reshape(target_shape=(timesteps, ch_rows * ch_cols, bands), name='eeg')(il)
     # initial convolution
-    ml = kl.Conv2D(filters=32, kernel_size=(5, 1), strides=(1, 1), activation='relu', name='conv', padding='same')(ml)
-    ml = kl.AveragePooling2D(pool_size=(2, 1))(ml)
-    # secondary convolution
-    ml = kl.Conv2D(filters=64, kernel_size=(5, 1), strides=(1, 1), activation='relu', name='conv', padding='same')(ml)
-    ml = kl.AveragePooling2D(pool_size=(2, 1))(ml)
+    _f = 8
+    _k = (5, 1)
+    ml = kl.Conv2D(filters=_f, kernel_size=_k, name=f'conv_1')(ml)
+    # dense blocks
+    for i in range(2):
+        _l = 4 * (i + 1)
+        ml = DB(name=i + 1, layers=_l, filters=_f, kernel_size=_k)(ml)
+        ml = TB(name=i + 1, filters=_f * _l)(ml)
     # convert to capsule domain
-    ml = ConvCaps2D(filters=16, filter_dims=8, kernel_size=(5, 1), strides=(2, 1), name='conv_caps')(ml)
+    ml = ConvCaps2D(filters=8, filter_dims=4, kernel_size=(5, 1), strides=(2, 1), name='conv_caps')(ml)
     ml = kl.Lambda(squash)(ml)
     # dense capsule layer with dynamic routing
-    ml = DenseCaps(caps=2, caps_dims=16, routing_iter=3, name='dense_caps')(ml)
+    ml = DenseCaps(caps=2, caps_dims=8, routing_iter=3, name='dense_caps')(ml)
     ml = kl.Lambda(squash)(ml)
     # select capsule with highest activity
     cl = kl.Lambda(mask_cid)(ml)
