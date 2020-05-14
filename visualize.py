687c3a2991d0efebec7c5d4237d0adeb56f26dca
yasith <yasith@cs.odu.edu>
2020-05-14 16:25:15 -0400

    update visualization script and figure

--- visualize.py
+++ visualize.py
@@ -1,43 +1,40 @@
+#!/usr/bin/env python3
+
 from typing import List
 
-import matplotlib
 import numpy as np
-from matplotlib import pyplot as plt, animation
+from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
-from matplotlib.image import AxesImage
 
-from info import participants, SLICE_SHAPE_BANDS
+from info import SLICE_SHAPE_BANDS, participants
 
-matplotlib.use("TkAgg")
-
-band_names = ['delta', 'theta', 'alpha', 'beta', 'gamma']
+band_names = ['delta', 'theta', 'alpha', 'beta', 'gamma'][::-1]
 
 if __name__ == '__main__':
-    T, H, W, C = SLICE_SHAPE_BANDS
+    T, H, W, R = SLICE_SHAPE_BANDS
     data = np.load('data/data-processed-bands.npz')
 
     # plot configuration
-    L = 5
-    fig, axs = plt.subplots(L, C, sharex='all', sharey='all', figsize=(6, 5))  # type: Figure, List[List[Axes]]
-    fig.subplots_adjust(wspace=0.02, hspace=0.04)
-    fig.suptitle(f'({H}x{W}) Power Spectrum of {L} Participants over {T} Timesteps')
-    ims = []  # type: List[AxesImage]
-
-    for i in range(L):
-        axs[i][0].set_ylabel(participants[i])
-        for j in range(C):
-            axs[0][j].set_title(band_names[j])
-            im = axs[i][j].imshow(np.zeros((H, W)), cmap='gray', vmin=0, vmax=1, animated=True)
-            axs[i][j].set_xticks([])
-            axs[i][j].set_yticks([])
-            ims.append(im)
-
-    def update_fig(_t):
-        for _i, _im in enumerate(ims):
-            _im.set_array(data[f'{participants[_i // C]}_x'][0, _t, :, :, _i % C])
-            yield _im
-
-
-    ani = animation.FuncAnimation(fig, update_fig, frames=150, blit=True)
-    fig.show()
+    step = 5
+    C = 5
+    for p in participants:
+        fig, axs = plt.subplots(R, C, sharex='all', sharey='all', figsize=(9, 6))  # type: Figure, List[List[Axes]]
+        fig.suptitle(f'Normalized Power Spectrum - Participant {p}')
+        # row
+        for r in range(R):
+            axs[r][0].set_ylabel(band_names[r])
+            # column
+            for c in range(C):
+                t = c * step
+                axs[-1][c].set_xlabel(f'{t * 0.2:.0f}')
+                # power spectrum
+                ax = axs[r][c]
+                im = ax.imshow(data[f'{p}_x'][0, t, :, :, r], cmap='inferno', vmin=0, vmax=1)
+                ax.set_xticks([])
+                ax.set_yticks([])
+        fig.subplots_adjust(wspace=0.05, hspace=0.05)
+        fig.colorbar(im, ax=np.ravel(axs).tolist())
+        fig.savefig(f'figures/{p}.pdf', bbox_inches='tight')
+        print(f'{p} - OK')
+    print('DONE')
