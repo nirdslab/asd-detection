e6f79e4c40692ae5a3847454e6c481b1fad353ba
yasith <yasith@cs.odu.edu>
2020-05-23 13:48:28 -0400

    update models

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from typing import Tuple
 
+import tensorflow as tf
 from capsnet.layers import ConvCaps2D, DenseCaps
 from capsnet.nn import squash, norm, mask_cid
 from tensorflow.keras import layers as kl, models as km
@@ -58,40 +59,107 @@ class DenseBlock(kl.Layer):
         return x
 
 
+def _CONV(timesteps, ch_rows, ch_cols, bands):
+    def call(il):
+        ml = kl.Reshape((timesteps, ch_rows * ch_cols, bands))(il)
+        # == define variables ==
+        _f = 8  # filters per convolution
+        _l = 4  # convolutions per block
+        _n = 4  # dense + transition blocks
+        _k = (4, 1)  # size of convolution kernel
+        # == intermediate layer(s) ==
+        # initial convolution
+        ml = kl.Conv2D(filters=_f, kernel_size=_k)(ml)
+        ml = kl.BatchNormalization()(ml)
+        ml = kl.ReLU()(ml)
+        # stack dense blocks and transition blocks
+        for i in range(_n):
+            ml = DenseBlock(conv=_l, filters=_f, kernel_size=_k)(ml)
+            ml = TransitionBlock(filters=ml.shape[-1])(ml)
+        # flatten
+        ml = kl.Flatten()(ml)
+        return ml
+
+    return call
+
+
+def _LSTM(timesteps, ch_rows, ch_cols, bands):
+    def call(il):
+        ml = kl.Reshape((timesteps, ch_rows * ch_cols * bands))(il)
+        # == intermediate layer(s) ==
+        B = 32
+        N = 4
+        seq = []
+        # stack LSTM blocks
+        for i in range(N):
+            ml = kl.LSTM(B, return_sequences=True, dropout=DROPOUT)(ml)
+            seq.append(ml)
+            if i > 0: ml = kl.Concatenate()([*seq])
+        # convolution-lstm layer 3
+        ml = kl.LSTM(B, dropout=DROPOUT)(ml)
+        ml = kl.Dense(B, activation='relu')(ml)
+        return ml
+
+    return call
+
+
+def _CAPS(timesteps, ch_rows, ch_cols, bands):
+    def call(il):
+        ml = kl.Reshape(target_shape=(timesteps, ch_rows * ch_cols, bands))(il)
+        # == define variables ==
+        _f = 8  # filters per convolution
+        _l = 4  # convolutions per block
+        _n = 2  # dense + transition blocks
+        _d0 = 4  # start capsule dimensions
+        _s = (2, 1)  # conv capsule stride
+        _d1 = 8  # final capsule dimensions
+        _r = 3  # dynamic routing iterations
+        _k = (4, 1)  # size of convolution kernel
+        # == intermediate layer(s) ==
+        # initial convolution
+        ml = kl.Conv2D(filters=_f, kernel_size=_k)(ml)
+        ml = kl.BatchNormalization()(ml)
+        ml = kl.ReLU()(ml)
+        # stack dense blocks and transition blocks
+        for i in range(_n):
+            ml = DenseBlock(conv=_l, filters=_f, kernel_size=_k)(ml)
+            ml = TransitionBlock(filters=ml.shape[-1])(ml)
+        # convert to capsule domain
+        ml = ConvCaps2D(filters=_f, filter_dims=_d0, kernel_size=_k, strides=_s)(ml)
+        ml = kl.Lambda(squash)(ml)
+        # dense capsule layer with dynamic routing
+        ml = DenseCaps(caps=2, caps_dims=_d1, routing_iter=_r)(ml)
+        ml = kl.Lambda(squash)(ml)
+        return ml
+
+    return call
+
+
+def _MLP(features):
+    def call(il):
+        # == intermediate layer(s) ==
+        ml = kl.Dense(16, activation='relu', kernel_regularizer=REG)(il)
+        ml = kl.Dense(32, activation='relu', kernel_regularizer=REG)(ml)
+        ml = kl.Dense(64, activation='relu', kernel_regularizer=REG)(ml)
+        return ml
+
+    return call
+
+
 def CONV(eeg_shape: Tuple):
     """
     Generate Convolution Model for EEG data
 
-    :param eeg_shape
-    :return:
+    :param eeg_shape: Shape of EEG input
+    :return: CONV model
     """
     # == input layer(s) ==
-    [timesteps, ch_rows, ch_cols, bands] = eeg_shape
-    il = kl.Input(shape=(timesteps, ch_rows, ch_cols, bands))
-    ml = kl.Reshape((timesteps, ch_rows * ch_cols, bands))(il)
-
-    # == define variables ==
-    _f = 8  # filters per convolution
-    _l = 4  # convolutions per block
-    _n = 4  # dense + transition blocks
-    _k = (4, 1)  # size of convolution kernel
-
-    # == intermediate layer(s) ==
-    # initial convolution
-    ml = kl.Conv2D(filters=_f, kernel_size=_k)(ml)
-    ml = kl.BatchNormalization()(ml)
-    ml = kl.ReLU()(ml)
-    # stack dense blocks and transition blocks
-    for i in range(_n):
-        ml = DenseBlock(conv=_l, filters=_f, kernel_size=_k)(ml)
-        ml = TransitionBlock(filters=ml.shape[-1])(ml)
-    # global average pooling
-    ml = kl.Flatten()(ml)
-
+    il = kl.Input(shape=eeg_shape)
+    # == model layer(s) ==
+    ml = _CONV(*eeg_shape)(il)
     # == output layer(s) ==
     label = kl.Dense(2, activation='softmax', kernel_regularizer=REG, name='l')(ml)
     score = kl.Dense(1, kernel_regularizer=REG, name='s')(ml)
-
     # == create and return model ==
     return km.Model(inputs=il, outputs=[label, score], name='CONV')
 
@@ -100,31 +168,16 @@ def LSTM(eeg_shape: Tuple):
     """
     Generate LSTM Model for EEG data
 
-    :param eeg_shape:
-    :return:
+    :param eeg_shape: Shape of EEG input
+    :return: LSTM model
     """
     # == input layer(s) ==
-    [timesteps, ch_rows, ch_cols, bands] = eeg_shape
-    il = kl.Input(shape=(timesteps, ch_rows, ch_cols, bands))
-    ml = kl.Reshape((timesteps, ch_rows * ch_cols * bands))(il)
-
-    # == intermediate layer(s) ==
-    B = 32
-    N = 4
-    seq = []
-    # stack LSTM blocks
-    for i in range(N):
-        ml = kl.LSTM(B, return_sequences=True, dropout=DROPOUT)(ml)
-        seq.append(ml)
-        if i > 0: ml = kl.Concatenate()([*seq])
-    # convolution-lstm layer 3
-    ml = kl.LSTM(B, dropout=DROPOUT)(ml)
-    ml = kl.Dense(B, activation='relu')(ml)
-
+    il = kl.Input(shape=eeg_shape)
+    # == model layer(s) ==
+    ml = _LSTM(*eeg_shape)(il)
     # == output layer(s) ==
     label = kl.Dense(2, activation='softmax', kernel_regularizer=REG, name='l')(ml)
     score = kl.Dense(1, kernel_regularizer=REG, name='s')(ml)
-
     # == create and return model ==
     return km.Model(inputs=il, outputs=[label, score], name='LSTM')
 
@@ -133,46 +186,18 @@ def CAPS(eeg_shape: Tuple):
     """
     Generate Capsule Model for EEG data
 
-    :param eeg_shape:
-    :return:
+    :param eeg_shape: Shape of EEG input
+    :return: CAPS model
     """
     # == input layer(s) ==
-    [timesteps, ch_rows, ch_cols, bands] = eeg_shape
-    il = kl.Input(shape=(timesteps, ch_rows, ch_cols, bands))
-    ml = kl.Reshape(target_shape=(timesteps, ch_rows * ch_cols, bands))(il)
-
-    # == define variables ==
-    _f = 8  # filters per convolution
-    _l = 4  # convolutions per block
-    _n = 2  # dense + transition blocks
-    _d0 = 4  # start capsule dimensions
-    _s = (2, 1)  # conv capsule stride
-    _d1 = 8  # final capsule dimensions
-    _r = 3  # dynamic routing iterations
-    _k = (4, 1)  # size of convolution kernel
-
-    # == intermediate layer(s) ==
-    # initial convolution
-    ml = kl.Conv2D(filters=_f, kernel_size=_k)(ml)
-    ml = kl.BatchNormalization()(ml)
-    ml = kl.ReLU()(ml)
-    # stack dense blocks and transition blocks
-    for i in range(_n):
-        ml = DenseBlock(conv=_l, filters=_f, kernel_size=_k)(ml)
-        ml = TransitionBlock(filters=ml.shape[-1])(ml)
-    # convert to capsule domain
-    ml = ConvCaps2D(filters=_f, filter_dims=_d0, kernel_size=_k, strides=_s)(ml)
-    ml = kl.Lambda(squash)(ml)
-    # dense capsule layer with dynamic routing
-    ml = DenseCaps(caps=2, caps_dims=_d1, routing_iter=_r)(ml)
-    ml = kl.Lambda(squash)(ml)
+    il = kl.Input(shape=eeg_shape)
+    # == model layer(s) ==
+    ml = _CAPS(*eeg_shape)(il)
     # select capsule with highest activity
     cl = kl.Lambda(mask_cid)(ml)
-
     # == output layer(s) ==
     label = kl.Lambda(norm, name='l')(ml)
     score = kl.Dense(1, name='s')(cl)
-
     # == create and return model ==
     return km.Model(inputs=il, outputs=[label, score], name='CAPS')
 
@@ -181,32 +206,89 @@ def MLP(irt_shape: Tuple):
     """
     Generate MLP for Thermal Data
 
-    :param irt_shape:
+    :param irt_shape: Shape of IRT input
+    :return: MLP model
     """
     # == input layer(s) ==
     il = kl.Input(shape=irt_shape)
-    ml = il
-
-    # == intermediate layer(s) ==
-    ml = kl.Dense(16, activation='relu', kernel_regularizer=REG)(ml)
-    ml = kl.Dense(32, activation='relu', kernel_regularizer=REG)(ml)
-    ml = kl.Dense(64, activation='relu', kernel_regularizer=REG)(ml)
-
+    # == model layer(s) ==
+    ml = _MLP(*irt_shape)(il)
     # == output layer(s) ==
     label = kl.Dense(2, activation='softmax', kernel_regularizer=REG, name='l')(ml)
     score = kl.Dense(1, kernel_regularizer=REG, name='s')(ml)
-
     # == create and return model ==
     return km.Model(inputs=il, outputs=[label, score], name='MLP')
 
 
 def CONV_MLP(eeg_shape: Tuple, irt_shape: Tuple):
-    return
+    """
+    Generate Convolution + MLP model for EEG and IRT data
+
+    :param eeg_shape: Shape of EEG input
+    :param irt_shape: Shape of IRT input
+    :return: CONV-MLP model
+    """
+    # == input layer(s) ==
+    il_eeg = kl.Input(shape=eeg_shape)
+    il_irt = kl.Input(shape=irt_shape)
+    # == model layer(s) ==
+    ml_eeg = _CONV(*eeg_shape)(il_eeg)
+    ml_irt = _MLP(*irt_shape)(il_irt)
+    ml = kl.Concatenate()([ml_eeg, ml_irt])
+    # == output layer(s) ==
+    label = kl.Dense(2, activation='softmax', kernel_regularizer=REG, name='l')(ml)
+    score = kl.Dense(1, kernel_regularizer=REG, name='s')(ml)
+    # == create and return model ==
+    return km.Model(inputs=[il_eeg, il_irt], outputs=[label, score], name='CONV-MLP')
 
 
 def LSTM_MLP(eeg_shape: Tuple, irt_shape: Tuple):
-    return
+    """
+    Generate LSTM + MLP model for EEG and IRT data
+
+    :param eeg_shape: Shape of EEG input
+    :param irt_shape: Shape of IRT input
+    :return: LSTM-MLP model
+    """
+    # == input layer(s) ==
+    il_eeg = kl.Input(shape=eeg_shape)
+    il_irt = kl.Input(shape=irt_shape)
+    # == model layer(s) ==
+    ml_eeg = _LSTM(*eeg_shape)(il_eeg)
+    ml_irt = _MLP(*irt_shape)(il_irt)
+    ml = kl.Concatenate()([ml_eeg, ml_irt])
+    # == output layer(s) ==
+    label = kl.Dense(2, activation='softmax', kernel_regularizer=REG, name='l')(ml)
+    score = kl.Dense(1, kernel_regularizer=REG, name='s')(ml)
+    # == create and return model ==
+    return km.Model(inputs=[il_eeg, il_irt], outputs=[label, score], name='LSTM-MLP')
 
 
 def CAPS_MLP(eeg_shape: Tuple, irt_shape: Tuple):
-    return
+    """
+    Generate Capsule + MLP model for EEG and IRT data
+
+    :param eeg_shape: Shape of EEG input
+    :param irt_shape: Shape of IRT input
+    :return: CONV-MLP model
+    """
+    # == input layer(s) ==
+    il_eeg = kl.Input(shape=eeg_shape)
+    il_irt = kl.Input(shape=irt_shape)
+    # == model layer(s) ==
+    ml_eeg = _CAPS(*eeg_shape)(il_eeg)
+    ml_irt = _MLP(*irt_shape)(il_irt)
+    # convert IRT data to capsule domain, with same shape as ml_eeg
+    ml_irt = kl.Dense(tf.reduce_prod(ml_eeg.shape[1:]))(ml_irt)
+    ml_irt = kl.Reshape(ml_eeg.shape[1:])(ml_irt)
+    ml_irt = kl.Lambda(squash)(ml_irt)
+    # vector addition of ml_eeg and ml_irt
+    ml = kl.Add()([ml_eeg, ml_irt])
+    ml = kl.Lambda(squash)(ml)
+    # select capsule with highest activity
+    cl = kl.Lambda(mask_cid)(ml)
+    # == output layer(s) ==
+    label = kl.Lambda(norm, name='l')(ml)
+    score = kl.Dense(1, name='s')(cl)
+    # == create and return model ==
+    return km.Model(inputs=[il_eeg, il_irt], outputs=[label, score], name='CAPS-MLP')
